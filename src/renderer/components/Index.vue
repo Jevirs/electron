29af4a2b1de57de65fa1685b1b274006e20b29f4
Jervis <jevirs@foxmail.com>
2018-05-10 15:28:24 +0800

    init rep

---
+++ src/renderer/components/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <p>Version:</p>
+      <p>{{version}}</p>
+
+      <button @click="update">UPDATE</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+      return{
+          version: '0.0.1'
+      }
+  },
+  methods:{
+      update(){
+          console.log('update')
+      }
+  }
+}
+</script>
+
